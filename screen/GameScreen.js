cd62cc73f328a94bacab190d86f67e02d1f4b147
nullmicgo <nullmicog@gmail.com>
2019-11-17 17:35:08 +0800

    Adding Icon for the lower and greater button

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet, Button, Alert} from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import DefaultStyles from '../constants/default-styles';
+import MainButton from '../components/MainButton';
 
+import { Ionicons} from '@expo/vector-icons';
 
 const generateRandomBetween = (min, max, exclude) => {
      min = Math.ceil(min);
@@ -60,8 +62,12 @@ const GameScreen = props => {
             <Text style={DefaultStyles.title}>Opponent's Guest</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')}></Button>
-                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')}>></Button>                
+                <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                    <Ionicons name="md-remove" size={24}  color="white"  />
+                </MainButton>
+                <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                     <Ionicons name="md-add" size={24} color="white"  />    
+                </MainButton>                
             </Card>
         </View>
     )
@@ -77,7 +83,8 @@ const styles = StyleSheet.create({
         flexDirection:"row",
         justifyContent:'space-around',
         marginTop: 20,
-        width: '80%'
+        width:400,
+        maxWidth: '90%'
     },
 });
 

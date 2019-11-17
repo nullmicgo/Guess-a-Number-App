1d36c770e2da7c6277e9170b899f16f082db85c4
nullmicgo <nullmicog@gmail.com>
2019-11-17 12:17:34 +0800

    Adding Win condition with useEffect

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef} from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import { View, Text, StyleSheet, Button, Alert} from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -22,8 +22,19 @@ const GameScreen = props => {
         generateRandomBetween(1, 100, props.userChoice)
     );
 
+    const [rounds, setRounds] = useState(0);
+
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props;
+
+    useEffect( ()=>{
+        //after render
+        if(currentGuess === userChoice){
+            onGameOver(rounds);
+        }
+    }, [currentGuess,userChoice,onGameOver]);
 
     const nextGuessHandler = direction =>{  
         if(( direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess >props.userChoice )){
@@ -40,6 +51,7 @@ const GameScreen = props => {
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
+        setRounds(currentRounds => currentRounds +1);
     };
 
     return (

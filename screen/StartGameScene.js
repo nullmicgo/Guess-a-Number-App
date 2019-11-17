6f6bd61b144f7477e346667cdb00bc2aa75a7c0c
nullmicgo <nullmicog@gmail.com>
2019-11-17 20:13:06 +0800

    Controling Orientation and adding keyboard avoiding view

--- screen/StartGameScene.js
+++ screen/StartGameScene.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput, Button, 
         TouchableWithoutFeedback, Keyboard, Alert,
-        Dimensions } from 'react-native';
+        Dimensions , ScrollView, KeyboardAvoidingView} from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/colors';
@@ -52,22 +52,26 @@ const StartGameScreen = props =>{
     }
 
     return (
-        <TouchableWithoutFeedback onPress={()=>{ 
-            Keyboard.dismiss();
-          }}>
-            <View style={styles.screen}>
-                <TitleText style={styles.title}>Start a New Game</TitleText>
-                <Card style={styles.inputContainer}>
-                        <BodyText>Select a Number</BodyText>
-                        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} value={enteredValue} onChangeText={numberInputHandler}  />
-                        <View style={styles.buttonContainer}>
-                            <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.accent} /></View>
-                            <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} /></View>   
+        <ScrollView>
+            <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={30}>
+                    <TouchableWithoutFeedback onPress={()=>{ 
+                        Keyboard.dismiss();
+                    }}>
+                        <View style={styles.screen}>
+                            <TitleText style={styles.title}>Start a New Game</TitleText>
+                            <Card style={styles.inputContainer}>
+                                    <BodyText>Select a Number</BodyText>
+                                    <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} value={enteredValue} onChangeText={numberInputHandler}  />
+                                    <View style={styles.buttonContainer}>
+                                        <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.accent} /></View>
+                                        <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} /></View>   
+                                    </View>
+                            </Card>
+                            {confirmedOutput}
                         </View>
-                </Card>
-                {confirmedOutput}
-            </View>
-        </TouchableWithoutFeedback>
+                    </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </ScrollView>
     );
 };
 

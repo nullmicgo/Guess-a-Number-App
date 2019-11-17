1d53bee3905b773184190d09288bd2f72b7d8e24
nullmicgo <nullmicog@gmail.com>
2019-11-17 12:53:18 +0800

    Adding Custom Fonts and AppLoading

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
 import  Header  from './components/Header';
 import StartGameScreen from './screen/StartGameScene';
 import GameScreen from './screen/GameScreen';
-import GameOverScreen from './screen/GameOver';
+import GameOverScreen from './screen/GameOverScreen';
+
+const fetchFonts = () =>{
+   return  Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+}
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if(!dataLoaded){
+    return <AppLoading 
+            startAsync={fetchFonts} 
+            onFinish={() => setDataLoaded(true)} 
+            onError={(err)=> console.log(err)}
+            />;
+  }
+
+
+  const configureNewGameHandler = () =>{
+    setGuessRounds(0);
+    setUserNumber(null);
+    
+  }
 
 
   const startGameHandler = (selectedNumber) =>{
       setUserNumber(selectedNumber);
-      setGuessRounds(0);
   };
 
   const gameOverHandler = numOfRounds =>{
@@ -24,7 +49,11 @@ export default function App() {
   if(userNumber  &&  guessRounds <= 0 ) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
   }else if (guessRounds > 0){
-    content = <GameOverScreen />;
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />;
+
+
+    
+
   }
 
   return (

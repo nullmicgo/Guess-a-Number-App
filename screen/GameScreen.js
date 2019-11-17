d75047eba1181e0a694392378e4ca9f6204cbc62
nullmicgo <nullmicog@gmail.com>
2019-11-17 19:38:24 +0800

    Adding If checking for dimension

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect} from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList} from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList, Dimensions} from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import DefaultStyles from '../constants/default-styles';
@@ -65,6 +65,12 @@ const GameScreen = props => {
 
     };
 
+    let listContainerStyle = styles.listContainer;
+    if(Dimensions.get('window').width < 350){
+        listContainerStyle = styles.listContainerBig;
+    }
+
+
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>Opponent's Guest</Text>
@@ -77,7 +83,7 @@ const GameScreen = props => {
                      <Ionicons name="md-add" size={24} color="white"  />    
                 </MainButton>                
             </Card>
-            <View style={styles.listContainer}>
+            <View style={listContainerStyle}>
                 {/* <ScrollView contentContainerStyle={styles.list}>
                     {pastGuess.map( (guess, index) => (
                         renderListItem(guess, pastGuess.length - index)
@@ -107,7 +113,7 @@ const styles = StyleSheet.create({
     buttonContainer:{
         flexDirection:"row",
         justifyContent:'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width:400,
         maxWidth: '90%'
     },
@@ -123,6 +129,10 @@ const styles = StyleSheet.create({
     },
     listContainer:{
         width: '60%',
+        flex: 1
+    },
+    listContainerBig:{
+        width: '80%', 
         flex: 1
     },
     list:{

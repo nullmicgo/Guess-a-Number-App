155b7bf2c44bb354e402fef4f1fe99894a7c202f
nullmicgo <nullmicog@gmail.com>
2019-11-17 19:23:30 +0800

    Fixing Repository issjue

--- screen/StartGameScene.js
+++ screen/StartGameScene.js
@@ -81,8 +81,9 @@ const styles = StyleSheet.create({
         fontFamily:'open-sans-bold'
     },
     inputContainer:{
-        width: 300,
-        maxWidth: '80%',
+        minWidth: 300,
+        width: '80%',
+        maxWidth: '95%',
         alignItems: 'center',
     },
     buttonContainer:{

5f3fb35f7607a3251d9d2e422d5193c41a484583
nullmicgo <nullmicog@gmail.com>
2019-11-17 22:02:24 +0800

    Adding Screen Locking

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -6,7 +6,7 @@ import DefaultStyles from '../constants/default-styles';
 import MainButton from '../components/MainButton';
 import { Ionicons} from '@expo/vector-icons';
 import BodyText from '../components/BodyText';
-
+import { ScreenOrientation } from 'expo';
 
 const generateRandomBetween = (min, max, exclude) => {
      min = Math.ceil(min);
@@ -29,6 +29,12 @@ const renderListItem = (listLength , itemData) =>(
 
 
 const GameScreen = props => {
+
+    //If we need to lock the Orientation
+    //ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+
+    
+
     const initialGuess =  generateRandomBetween(1, 100, props.userChoice);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [pastGuess, setPastGuess] = useState([initialGuess.toString()]);

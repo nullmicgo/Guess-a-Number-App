e11b10400bbc5e682e2d6b7424b8eb5489ed326e
nullmicgo <nullmicog@gmail.com>
2019-11-18 11:01:16 +0800

    Working with Platform.select and Platform in if check

--- components/TitleText.js
+++ components/TitleText.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet , Platform } from 'react-native';
 
 const TitleText = props => <Text style={{...styles.title, ...props.style }}>{props.children}</Text>
 
